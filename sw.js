559a293e28eea2a796e0283998ac817e8d6ec597
cheikhaidev-ui <cheikh.ia.dev@gmail.com>
2026-05-17 21:31:54 +0000

    ajout PWA manifest.json sw.js installation carte sur mobile

---
+++ sw.js
@@ -0,0 +1,15 @@
+var CACHE = 'carteviz-v1';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.add('./carte.html'); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
+});
